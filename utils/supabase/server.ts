09b9fb7655539a7c1ecbde5bf738e68fe959639d
kylerobins <robinsmicky@gmail.com>
2025-03-12 10:35:54 +0300

    ssr implimentations

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,7 +1,9 @@
+"use server";
+
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
-export function createClient(cookieStore: ReturnType<typeof cookies>) {
+export async function createClient(cookieStore: ReturnType<typeof cookies>) {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
